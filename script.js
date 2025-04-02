e44837e2538c67df4e0045317e398253cd59e757
devashish-hp <98695878+devashish-hp@users.noreply.github.com>
2025-04-02 17:10:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById("dark-mode-toggle");
-    
-    toggleButton.addEventListener("click", function() {
-        document.body.classList.toggle("dark-mode");
-        if (document.body.classList.contains("dark-mode")) {
-            toggleButton.textContent = "??";
-        } else {
-            toggleButton.textContent = "??";
-        }
-    });
+/* script.js (Dark Mode Toggle) */
+document.getElementById("dark-mode-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
 });
